602aaccedc3b2f2df06010ee1b43c9846d7b5a8b
Северин Цяпка <tsyapkaseveryn@gmail.com>
2018-07-15 18:09:15 +0000

    QApplication.java edited online with Bitbucket

--- src/main/java/com/queue/q/QApplication.java
+++ src/main/java/com/queue/q/QApplication.java
@@ -3,19 +3,15 @@ package com.queue.q;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 @SpringBootApplication
 public class QApplication {
-    static Queue<Request> mobileQueue = new PriorityQueue<>(new com.queue.q.Comparator());
-    static Queue<Request> deviceQueue = new PriorityQueue<>(new com.queue.q.Comparator());
-    static Queue<Request> serverQueue = new PriorityQueue<>(new com.queue.q.Comparator());
-    static Queue<Request> videoQueue = new PriorityQueue<>(new com.queue.q.Comparator());
 
-    public static void main(String[] args) {
-        SpringApplication.run(QApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(QApplication.class, args);
+
+
+
+	}
 
 
 }
