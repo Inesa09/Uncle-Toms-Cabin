8fefa5f2a66dedd2e79279be19f080413b830b68
Северин Цяпка <tsyapkaseveryn@gmail.com>
2018-09-11 18:02:52 +0000

    Request.java

--- src/main/java/com/queue/q/Request.java
+++ src/main/java/com/queue/q/Request.java
@@ -4,17 +4,28 @@ public class Request {
     private byte serviceId;
     private byte priority;
     private int type;
+    private boolean locked = false;
+    private int timelocked = 0;
     private Object request;
 
     public Request() {
         super();
     }
 
-    public Request(byte serviceId, byte priority, int type, Object request){
+    public Request(byte serviceId, byte priority, int type, boolean locked, Object request){
         this.serviceId = serviceId;
         this.priority = priority;
         this.type = type;
         this.request = request;
+        this.locked=locked;
+    }
+    public Request(byte serviceId, byte priority, int type, boolean locked, int timelocked, Object request){
+        this.serviceId = serviceId;
+        this.priority = priority;
+        this.type = type;
+        this.request = request;
+        this.locked = locked;
+        this.timelocked = timelocked;
     }
 
     public Request(byte serviceId, byte priority, Object request){
@@ -39,6 +50,10 @@ public class Request {
         return request;
     }
 
+    public boolean isLocked() { return locked; }
+
+    public int getTimelocked() { return timelocked; }
+
     public void setServiceId(byte serviceId) {
         this.serviceId = serviceId;
     }
@@ -46,11 +61,20 @@ public class Request {
     public void setPriority(byte priority) {
         this.priority = priority;
     }
+
     public void setType(int type) {
         this.type = type;
     }
 
     public void setRequest(Object request) {
         this.request = request;
+    }
+
+    public void setIslocked(boolean locked) { 
+        this.locked = locked; 
+    }
+
+    public void setTimelocked(int timelocked) { 
+        this.timelocked = timelocked; 
     }
 }
