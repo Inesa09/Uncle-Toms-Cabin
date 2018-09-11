9c379b2483d6180c3580b202ad4f51ac02b84991
Северин Цяпка <tsyapkaseveryn@gmail.com>
2018-09-11 17:59:16 +0000

    IQueue.java

--- src/main/java/com/queue/q/Queue/IQueue.java
+++ src/main/java/com/queue/q/Queue/IQueue.java
@@ -5,6 +5,8 @@ import com.queue.q.Request;
 public interface IQueue {
 
     void setRequest (Request request);
+    void lock (Lock lock);
+    void unlock (Lock lock);
     Request getRequest();
     boolean isEmpty();
 }
