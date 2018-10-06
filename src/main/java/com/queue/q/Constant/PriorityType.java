b6f839b1b3c10b08b967d4077ab4b20345fe382f
Северин Цяпка <tsyapkaseveryn@gmail.com>
2018-10-06 22:20:00 +0000

    Change NormalPriority name

--- src/main/java/com/queue/q/Constant/PriorityType.java
+++ src/main/java/com/queue/q/Constant/PriorityType.java
@@ -2,7 +2,7 @@ package com.queue.q.Constant;
 
 public class PriorityType {
     public static final int LOWPRIORITY = 1;
-    public static final int NORMALPRIOPITY = 2;
+    public static final int NORMALPRIORITY = 2;
     public static final int HIGHPRIORITY = 3;
 
     private PriorityType(){};
