b0e6d0a44eb213b40ce8f70e07659317192a7ed1
Inesa09 <inesusja@gmail.com>
2018-10-12 21:35:50 +0300

    fix bug in filling queues with unexecuted requests

--- src/main/java/com/queue/ApplicationStartup.java
+++ src/main/java/com/queue/ApplicationStartup.java
@@ -2,15 +2,13 @@ package com.queue;
 
 import com.queue.db.service.IRequestService;
 import com.queue.q.Constant.ServiceID;
+import com.queue.q.LinkedQueuesRealisation.LinkedQueue;
 import com.queue.q.Queue.DeviceQueue;
-import com.queue.q.Queue.IQueue;
 import com.queue.q.Queue.VideoQueue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
@@ -29,8 +27,8 @@ public class ApplicationStartup implements ApplicationListener<ApplicationReadyE
     }
 
     private void fillQueues(){
-        List<IQueue> queues = requestService.getQueuesWithUnexecutedRequests();
-        deviceQueue = (DeviceQueue) queues.get(ServiceID.DEVICE_ID - 1);
-        videoQueue = (VideoQueue) queues.get(ServiceID.VIDEO_ID - 1);
+        LinkedQueue[] queues = requestService.getQueuesWithUnexecutedRequests();
+        deviceQueue.setQueue(queues[ServiceID.DEVICE_ID - 1]);
+        videoQueue.setQueue(queues[ServiceID.VIDEO_ID - 1]);
     }
 }
