9b282cdf17542d8dec541b6e1b5151aaf3ffdba3
Inesa09 <inesusja@gmail.com>
2018-11-16 03:53:48 +0200

    add class Response

---
+++ src/main/java/com/queue/queue/Response.java
@@ -0,0 +1,23 @@
+package com.queue.queue;
+
+public class Response {
+    private String guid;
+    int status;
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
+
