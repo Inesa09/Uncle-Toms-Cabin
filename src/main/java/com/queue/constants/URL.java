61cc09d2aa0b404408b55473c38a9326601ff702
Shuvi179 <feona9999@ukr.net>
2018-11-23 01:08:44 +0200

    Add UserService URL

--- src/main/java/com/queue/constants/URL.java
+++ src/main/java/com/queue/constants/URL.java
@@ -3,4 +3,5 @@ package com.queue.constants;
 public class URL {
     public static final String MOBILE_URL = "http://localhost:8080/mobile";
     public static final String WEB_URL = "http://localhost:8080/web";
+    public static final String USER_URL = "http://localhost:8080/web";
 }
